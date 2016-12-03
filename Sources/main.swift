925228f349edea28f592fb38d86b33ea406a1dac
Shibin <shibin1710@gmail.com>
2016-12-04 00:05:05 +0530

    defined constants

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,19 +2,63 @@ import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
 import MongoDB
+import Foundation
+
+// Routing Constants
+let signUpRoute = "/signup"
+let loginRoute = "/login"
+
+// Database Constants
+let mongoClient = "mongodb://shibin:shibin@ds151127.mlab.com:51127/transport"
+let dbName = "transport"
+let credentialCollection = "credentials"
+let mongoIDKey = "_id"
+
+// Server Constants
+let port:UInt16 = 8082
+
+// Error Constants
+let errorKey = "error"
+let errorCodeKey = "code"
+let errorDescriptionKey = "description"
+
+// Success Constants
+let successKey = "success"
+let successDictionaryKey = "responseDictionary"
+
+// Credentials Collection Names
+let emailKey = "email"
+let passwordKey = "password"
+let nameKey = "name"
+let phoneNumberKey = "phoneNumber"
+
+// Error Codes
+enum ErrorCode:Int {
+    case InvalidRequest = 1000
+    case DatabaseConnectionError = 1001
+    case UserAlreadyExists = 1002
+    case DatabaseInsertionError = 1003
+    case UserNotRegistered = 1004
+    case ParseError = 1005
+    case ServerError = 1006
+
+}
 
 // Create HTTP server.
 let server = HTTPServer()
 
 // Register your own routes and handlers
 var routes = Routes()
-routes.add(method: .post, uri: "/signup", handler: {
+
+/**************************** Signup Service *******************************/
+
+routes.add(method: .post, uri: signUpRoute, handler: {
     request, response in
     
-    guard let email = request.param(name: "email"), let password = request.param(name: "password"), let name = request.param(name: "name"), let phoneNumber = request.param(name: "phoneNumber") else {
+    if request.params().count != 4 {
         
-        let errorJSON = ["code" : "1000", "description" : "Invalid request."]
-        let responseJSON = ["success" : "false","error" : errorJSON] as [String : Any]
+        let errorJSON = [errorCodeKey : ErrorCode.InvalidRequest.rawValue, errorDescriptionKey : "Invalid request."] as [String : Any]
+        let responseJSON = [successKey : false ,errorKey : errorJSON] as [String : Any]
         
         do {
             try response.setBody(json: responseJSON)
@@ -25,15 +69,30 @@ routes.add(method: .post, uri: "/signup", handler: {
         response.completed()
         return
     }
-
-    let client = try! MongoClient(uri: "mongodb://shibin:shibin@ds151127.mlab.com:51127/transport")
-    let db = client.getDatabase(name: "transport")
+    
+    guard let email = request.param(name: emailKey), let password = request.param(name: passwordKey), let name = request.param(name: nameKey), let phoneNumber = request.param(name: phoneNumberKey) else {
+        
+        let errorJSON = [errorCodeKey : ErrorCode.InvalidRequest.rawValue, errorDescriptionKey : "Invalid request."] as [String : Any]
+        let responseJSON = [successKey : false ,errorKey : errorJSON] as [String : Any]
+        
+        do {
+            try response.setBody(json: responseJSON)
+        } catch {
+            print("Error : ",error)
+        }
+        
+        response.completed()
+        return
+    }
+    
+    let client = try! MongoClient(uri: mongoClient)
+    let db = client.getDatabase(name: dbName)
     
     // define collection
-    guard let collection = db.getCollection(name: "credentials") else {
+    guard let collection = db.getCollection(name: credentialCollection) else {
         
-        let errorJSON = ["code" : "1001", "description" : "Error connecting to database."]
-        let responseJSON = ["success" : "false","error" : errorJSON] as [String : Any]
+        let errorJSON = [errorCodeKey : ErrorCode.DatabaseConnectionError.rawValue, errorDescriptionKey : "Error connecting to database."] as [String : Any]
+        let responseJSON = [successKey : false,errorKey : errorJSON] as [String : Any]
         
         do {
             try response.setBody(json: responseJSON)
@@ -53,8 +112,13 @@ routes.add(method: .post, uri: "/signup", handler: {
         client.close()
     }
     
-    let filterBSON = try! BSON(json: "{\"email\" : \"\(email)\"}")
-    let filterResults = collection.find(query:filterBSON)
+    let filterBson = try! BSON(json: "{\"email\" : \"\(email)\"}")
+    
+    defer {
+        filterBson.close()
+    }
+    
+    let filterResults = collection.find(query:filterBson)
     
     // Initialize empty array to receive formatted results
     var responseData = [String]()
@@ -64,8 +128,8 @@ routes.add(method: .post, uri: "/signup", handler: {
     }
     
     guard responseData.count == 0 else {
-        let errorJSON = ["code" : "1002", "description" : "User already exist."]
-        let responseJSON = ["success" : "false","error" : errorJSON] as [String : Any]
+        let errorJSON = [errorCodeKey : ErrorCode.UserAlreadyExists.rawValue, errorDescriptionKey : "User already exist."] as [String : Any]
+        let responseJSON = [successKey : false, errorKey : errorJSON] as [String : Any]
         
         do {
             try response.setBody(json: responseJSON)
@@ -79,59 +143,101 @@ routes.add(method: .post, uri: "/signup", handler: {
     
     
     let bson = BSON()
-    bson.append(key: "email", string: email)
-    bson.append(key: "password", string: password)
-    bson.append(key: "name", string: name)
-    bson.append(key: "phoneNumber", string: phoneNumber)
-
+    
+    defer {
+        bson.close()
+    }
+    
+    bson.append(key: emailKey, string: email)
+    bson.append(key: passwordKey, string: password)
+    bson.append(key: nameKey, string: name)
+    bson.append(key: phoneNumberKey, string: phoneNumber)
+    
     let status:MongoResult = collection.insert(document: bson)
     
     switch status {
         
-        case .success:
+    case .success:
         
-            let userDetailJSON = ["name" : name, "email" : email, "phoneNumber" : phoneNumber]
-            let responseJSON = ["success" : "true","responseDictionary" : userDetailJSON] as [String : Any]
+        let userDetailJSON = [nameKey : name, emailKey : email, phoneNumberKey : phoneNumber]
+        let responseJSON = [successKey : true, successDictionaryKey : userDetailJSON] as [String : Any]
         
-            do {
-                try response.setBody(json: responseJSON)
-            } catch {
-                print("Error : ",error)
-            }
-        
-            response.completed()
-    
-        default:
-        
-            let errorJSON = ["code" : "1003", "description" : "Error while inserting to database."]
-            let responseJSON = ["success" : "false","error" : errorJSON] as [String : Any]
-        
-            do {
-                try response.setBody(json: responseJSON)
-            } catch {
-                print("Error : ",error)
-            }
-        
-            response.completed()
-            return
+        do {
+            try response.setBody(json: responseJSON)
+        } catch {
+            print("Error : ",error)
         }
+        
+        response.completed()
+        
+    default:
+        
+        let errorJSON = [errorCodeKey : ErrorCode.DatabaseInsertionError.rawValue, errorDescriptionKey : "Error while inserting to database."] as [String : Any]
+        let responseJSON = [successKey : false, errorKey : errorJSON] as [String : Any]
+        
+        do {
+            try response.setBody(json: responseJSON)
+        } catch {
+            print("Error : ",error)
+        }
+        
+        response.completed()
+        return
     }
-)
+})
+/**************************************************************************/
 
-routes.add(method: .post, uri: "/login", handler: {
+/**************************** Login Service *******************************/
+
+routes.add(method: .post, uri: loginRoute, handler: {
     request, response in
     
-    let username = request.param(name: "username")
-    let password = request.param(name: "password")
+    if request.params().count != 2 {
+        
+        let errorJSON = [errorCodeKey : ErrorCode.InvalidRequest.rawValue, errorDescriptionKey : "Invalid request."] as [String : Any]
+        let responseJSON = [successKey : false ,errorKey : errorJSON] as [String : Any]
+        
+        do {
+            try response.setBody(json: responseJSON)
+        } catch {
+            print("Error : ",error)
+        }
+        
+        response.completed()
+        return
+    }
     
-    //    let client = try! MongoClient(uri: "mongodb://localhost")
-    let client = try! MongoClient(uri: "mongodb://shibin:shibin@ds151127.mlab.com:51127/transport")
+    guard let email = request.param(name: emailKey), let password = request.param(name: passwordKey)else {
+        
+        let errorJSON = [errorCodeKey : ErrorCode.InvalidRequest.rawValue, errorDescriptionKey : "Invalid request."] as [String : Any]
+        let responseJSON = [successKey : false, errorKey : errorJSON] as [String : Any]
+        
+        do {
+            try response.setBody(json: responseJSON)
+        } catch {
+            print("Error : ",error)
+        }
+        
+        response.completed()
+        return
+    }
     
-    
-    let db = client.getDatabase(name: "transport")
+    let client = try! MongoClient(uri: mongoClient)
+    let db = client.getDatabase(name: dbName)
     
     // define collection
-    guard let collection = db.getCollection(name: "credentials") else {
+    guard let collection = db.getCollection(name: credentialCollection) else {
+        
+        let errorJSON = [errorCodeKey : ErrorCode.DatabaseConnectionError.rawValue, errorDescriptionKey : "Error connecting to database."] as [String : Any]
+        let responseJSON = [successKey : false, errorKey : errorJSON] as [String : Any]
+        
+        do {
+            try response.setBody(json: responseJSON)
+        } catch {
+            print("Error : ",error)
+        }
+        
+        response.completed()
         return
     }
     
@@ -143,39 +249,90 @@ routes.add(method: .post, uri: "/login", handler: {
         client.close()
     }
     
+    let bson = try! BSON(json: "{\"email\" : \"\(email)\",\"password\" : \"\(password)\"}")
     
-    // Perform a "find" on the perviously defined collection
+    defer {
+        bson.close()
+    }
     
-    let str = "username : \(username!)"
-    let bson = try! BSON(json: "{\"username\" : \"\(username!)\",\"password\" : \"\(password!)\"}")
-    let result = collection.find(query:bson)
+    let filterBson = BSON()
+    filterBson.append(key: mongoIDKey)
+    filterBson.append(key: passwordKey)
+    
+    defer {
+        filterBson.close()
+    }
+
+    let filterResults = collection.find(query: bson, fields:filterBson)
     
     // Initialize empty array to receive formatted results
     var responseData = [String]()
     
-    for x in result! {
-        responseData.append(x.asString)
+    for result in filterResults! {
+        responseData.append(result.asString)
     }
-    
-    // return a formatted JSON array.
-    //    let responseJSON = "{\"data\":[\(arr.joined(separator: ","))]}"
     
     if responseData.count == 0 {
-        response.appendBody(string: "{\"success\":false}")
-    } else {
-        response.appendBody(string: "{\"success\":true}")
         
+        let errorJSON = [errorCodeKey : ErrorCode.UserNotRegistered.rawValue, errorDescriptionKey : "User does not exist."] as [String : Any]
+        let responseJSON = [successKey : false, errorKey : errorJSON] as [String : Any]
+        
+        do {
+            try response.setBody(json: responseJSON)
+        } catch {
+            print("Error : ",error)
+        }
+        
+        response.completed()
+        return
+    } else {
+        
+        let loginResult = responseData.first
+        
+        guard let data = loginResult?.data(using: .utf8) else {
+            
+            let errorJSON = [errorCodeKey : ErrorCode.ParseError.rawValue, errorDescriptionKey : "Error in parsing data."] as [String : Any]
+            let responseJSON = [successKey : false, errorKey : errorJSON] as [String : Any]
+            
+            do {
+                try response.setBody(json: responseJSON)
+            } catch {
+                print("Error : ",error)
+            }
+            
+            response.completed()
+            return
+        }
+        
+        do {
+            var loginDictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+            
+            if (loginDictionary != nil) {
+                                
+                let responseJSON = [successKey : true, successDictionaryKey : loginDictionary] as [String : Any]
+                
+                do {
+                    try response.setBody(json: responseJSON)
+                } catch {
+                    print("Error : ",error)
+                }
+                
+                response.completed()
+                return
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
     }
-    
-    response.completed()
-    }
-)
+})
+
+/**************************************************************************/
 
 // Add the routes to the server.
 server.addRoutes(routes)
 
 // Set a listen port of 8181
-server.serverPort = 8082
+server.serverPort = port
 
 do {
     // Launch the HTTP server.
